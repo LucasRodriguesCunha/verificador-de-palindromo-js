4c2d37250afe6e7883e7fe09398148517979022d
Lucas Rodrigues Cunha <123827882+LucasRodriguesCunha@users.noreply.github.com>
2023-08-02 20:11:22 -0300

    Finished project

---
+++ script.js
@@ -0,0 +1,24 @@
+// Obtém os elementos HTML
+const botaoVerificar = document.querySelector("#botao-verificador");
+const palavraInput = document.querySelector("#palavra");
+const resultado = document.querySelector("#resultado");
+
+function verificarPalindromo() {
+    const palavra = palavraInput.value
+    const palavraInvertida = palavra.split("").reverse().join("")
+
+    if (palavra.toLowerCase() === palavraInvertida.toLowerCase()) {
+        resultado.textContent = `A palavra "${palavra}" é um palíndromo.`
+    } else {
+        resultado.textContent = `A palavra "${palavra}" não é um palíndromo.`
+    }
+}
+
+botaoVerificar.addEventListener("click", verificarPalindromo);
+
+palavraInput.addEventListener("keyup", (e) => {
+    if (e.key == "Enter") {
+        e.preventDefault();
+        verificarPalindromo();
+    }
+});
